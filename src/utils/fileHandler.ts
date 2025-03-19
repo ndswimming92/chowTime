80c6482815d4726e827a649edd43ad62fff98e3a
Nicholas Davidson <nicholas.davidson@daveramsey.com>
2025-03-19 13:48:01 -0500

    update readMeals to handle non-existant or empty file

--- src/utils/fileHandler.ts
+++ src/utils/fileHandler.ts
@@ -3,5 +3,10 @@ import type { BunFile } from 'bun';
 export async function readMealsJson(): Promise<any[]> {
   const filePath = `${import.meta.dir}/../data/meals.json`;
   const file: BunFile = Bun.file(filePath);
+
+  if (!(await file.exists())) {
+    return []
+  }
+
   return await file.json();
 }
