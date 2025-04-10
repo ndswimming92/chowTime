8d60008f1b2b18b7e24a535de77d33774780e580
Nicholas Davidson <nicholas.davidson@daveramsey.com>
2025-04-10 14:35:22 -0500

    updated step 2 to add a meal

--- src/utils/fileHandler.ts
+++ src/utils/fileHandler.ts
@@ -1,5 +1,6 @@
 import type { BunFile } from 'bun';
 import { writeFile } from 'fs/promises';
+import type { Meal } from '../types/meal';
 
 const filePath = `${import.meta.dir}/../data/meals.json`;
 
@@ -10,11 +11,28 @@ export async function readMealsJson(): Promise<any[]> {
     return [];
   }
 
-  return await file.json();
+  const data = await file.json();
+
+  // Check if the data is an array, and return it, else return an empty array
+  if (Array.isArray(data)) {
+    return data;
+  } else {
+    console.error(
+      'Data in meals.json is not an array, returning an empty array',
+    );
+    return [];
+  }
 }
 
-export async function addMeal(meal: any) {
+export async function addMeal(meal: Meal) {
   const meals = await readMealsJson(); // Read existing meals
+
+  // Ensure meals is an array before pushing
+  if (!Array.isArray(meals)) {
+    console.error('Meals data is not an array!');
+    return;
+  }
+
   meals.push(meal); // Add new meal
 
   // Write back to the file
